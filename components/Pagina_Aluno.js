6e3b43f39285bbf508b8907f7a08165090fa49c5
MateusMen <105519082+MateusMen@users.noreply.github.com>
2023-12-02 07:43:37 -0300

    Update Pagina_Aluno.js

--- components/Pagina_Aluno.js
+++ components/Pagina_Aluno.js
@@ -1,23 +1,47 @@
-import { Text, SafeAreaView,TouchableOpacity } from 'react-native';
-import Styles from './Styles'
+import { Text, SafeAreaView,TouchableOpacity  } from 'react-native';
+import Styles from '../Styles'
 import { useNavigation } from '@react-navigation/native';
+import {useAuth} from '../src/authDetails'
+import  AsyncStorage  from '@react-native-async-storage/async-storage'
+import { useEffect,useState } from 'react';
+
 
 
 export default function PaginaAluno() {
   const {navigate} = useNavigation()
+  const {signout} = useAuth()
   
+  
+  const [user, setUser] = useState()
+  
+
+  useEffect(() => {
+    AsyncStorage
+      .getItem('user')
+      .then(data => setUser(JSON.parse(data)));
+  }, [])
+
   return (
     <SafeAreaView style={Styles.container}>
       <Text style={Styles.paragraph}>
-        Pagina do Aluno
+        Bem vindo {user?.email}! 
       </Text>
+
+      
+
        <TouchableOpacity style={Styles.button}
       onPress={()=>navigate('MateriaAluno')}>
       <Text>Comecar quiz</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={Styles.button}>
+      <TouchableOpacity style={Styles.button}
+      onPress={()=>navigate('Ranking')}>
       <Text>ver o rank</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={Styles.button}
+      onPress={signout}>
+      <Text>sair</Text>
       </TouchableOpacity>
     </SafeAreaView>
   );
